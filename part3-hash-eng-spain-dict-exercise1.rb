26ad7f27743e4e9d2d6142f975b911dbc55dcaf1
baurzhaan <102077363+baurzhaan@users.noreply.github.com>
2024-01-26 14:42:21 +0000

    Part3, hashes, eng-spain dictionary exercise

---
+++ part3-hash-eng-spain-dict-exercise1.rb
@@ -0,0 +1,13 @@
+# Implement sophisticated English-Spanish dictionary, where one English word is represented by multiple Spanish translations.
+
+eng_spain_dict = {
+  'chair' => ['silla', 'sillon', 'asiento'],
+  'wall' => ['muro', 'barrera'],
+  'window' => ['ventana', 'vidriera', 'luneta'],
+  'door' => ['puerta'],
+  'come in' => ['adelante']
+}
+
+puts eng_spain_dict
+puts eng_spain_dict['door']
+puts eng_spain_dict['come in']
